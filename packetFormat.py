aa7e02fe7e6e0f7e9519d14d56a4622d06efc736
HarryAJ01 <91964772+HarryAJ01@users.noreply.github.com>
2022-05-06 23:45:37 +0100

    Update packetFormat.py

--- packetFormat.py
+++ packetFormat.py
@@ -1,105 +1,65 @@
-# Imports
 from bitarray import bitarray
 import rsa
 
-# Packet class
 class packetFormat:
-    # Constructor
-    def __init__(self, ack, rsa, fin, sequence, flags, key, body, isRSAKey):
-        # Fields
-        self.raw = None
-        self.encrypted_bytes = None
-        self.ack = ack
-        self.rsa = rsa
-        self.fin = fin
-        self.flags = flags
-        self.key = key
-        self.length = 0
-        self.sequence = sequence
-        self.body = body
-        self.isRSAKey = isRSAKey
+   
+    def __init__(self,sequence, ackFlag, rsaFlag, finFlag, extraFlags, key, payload):
+        header = None
 
-        # Processes the packet once fields are initalised
-        self.process_packet()
-
-    # Processes the packet
-    def process_packet(self):
-        # Calculate length
-        self.calculate_length()
-
-        # Convert to bytes
-        self.convert_to_bytes()
-
-        # Encrypt the packet
-        self.encrypt_packet()
-
-    # Calculates length
-    def calculate_length(self):
-        if self.isRSAKey:
-            self.length = 72
-        elif self.body != None:
-            self.length = 8 + len(bytes(str(self.body), 'ASCII'))
-        else:
-            self.length = 8
-
-    # Converts the packet to bytes
-    def convert_to_bytes(self):
         # Sequence
-        sequence_bytes = self.sequence.to_bytes(4, byteorder='big')
-        total = sequence_bytes
-
-        # Flags
-        total_flags = bitarray()
-
-        # ACK
-        if self.ack:
-            total_flags.append(1)
+        header = sequence.to_bytes(4, byteorder = 'big')
+        
+        # Flags 
+        flags = bitarray()
+        if(ackFlag == True):
+            flags.append(True)
         else:
-            total_flags.append(0)
+            flags.append(False)
 
-        # RSA
-        if self.rsa:
-            total_flags.append(1)
+        if(rsaFlag == True):
+            flags.append(True)
         else:
-            total_flags.append(0)
-
-        # FIN
-        if self.fin:
-            total_flags.append(1)
+            flags.append(False)
+        
+        if(finFlag == True):
+            flags.append(True)
         else:
-            total_flags.append(0)
+            flags.append(False)
 
-        # Flags
-        if self.flags != None:
-            for flag in self.flags:
-                if flag:
-                    total_flags.append(1)
-                else:
-                    total_flags.append(0)
+        if(extraFlags == None):
+            flags.extend([False, False, False, False, False, False, False, False, False, False, False, False, False])
+
+        flags = flags.tobytes()
+        header += flags
+
+        # Length
+        length = 8
+        if(rsaFlag == True):
+            length += 64
         else:
-            total_flags.extend([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+            if(payload != None):
+                length += len(bytes(str(payload), 'ascii'))
+        header += length.to_bytes(2, byteorder = 'big')
 
-        # Convert the bitarray to bytes
-        flag_bytes = total_flags.tobytes()
-        total = total + flag_bytes
+        # encoding body to bytes
+        body = b''
+        if(rsaFlag == True):
+            body = payload    #Key doesn't need converting
+        elif(payload != None):
+            body = bytes(str(payload), 'ASCII')
 
-        # Convert length to bytes
-        length_bytes = self.length.to_bytes(2, byteorder='big')
-        total = total + length_bytes
+        packet = header + body
 
-        # Convert body to bytes
-        if self.isRSAKey:
-            total = total + self.body
-        elif self.body != None:
-            self.body = bytes(str(self.body), 'ASCII')
+        # Only return variable
+        global encryptedBytes
 
-        # Set the field to the total
-        self.raw = total
-
-    # Encrypts the packet
-    def encrypt_packet(self):
-        if self.key == None:
-            self.encryptedBytes = self.raw
+        # Emcrypts the message usimg RSA if necessary
+        if key == None:
+            encryptedBytes = packet
         else:
-            encrypted_body = rsa.encrypt(self.body, self.key)
-            self.encryptedBytes = self.raw + encrypted_body
+            cipherText = rsa.encrypt(body, key)
+            encryptedBytes = header + cipherText
+
+    # Getter for message to be sent
+    def getEncryptedBytes(self):
+        return encryptedBytes
